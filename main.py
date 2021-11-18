bb1584a09036842e880a48defc27556fb5b6fe43
Kaenova Mahendra Auditama <kaenova@gmail.com>
2021-11-18 13:28:14 +0700

    Fix and add loggin

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ import os
 domain = {}
 
 if os.getenv("NGROK_AUTH") != "":
+    print("using ngrok for entry")
     ngrok.set_auth_token(os.getenv("NGROK_AUTH"))
     ngrok_tunnel = ngrok.connect(80)
-    domain["ngrok"] = os.getenv("NGROK_AUTH")
+    domain["ngrok"] = ngrok_tunnel.public_url
     
 if os.getenv("CUSTOM_DOMAIN") != "":
+    print("using custom domain for entry")
     domain["domain_address"] = os.getenv("CUSTOM_DOMAIN")
     
 if os.getenv("MACHINE_IP_ADDRESS") != "":
+    print("using machine ip for entry")
     domain["ip_machine"] = os.getenv("MACHINE_IP_ADDRESS")
     
 if len(domain) == 0:
